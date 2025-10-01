74d29cd64273a867f4f1586eb46c475e18d8a2c7
Kishore-m08 <kishoremarimuthu05@gmail.com>
2025-10-01 13:06:57 +0530

    Create Position.java

---
+++ marsrover/core/Position.java
@@ -0,0 +1,22 @@
+package marsrover.core;
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x; this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public Position move(int dx, int dy) {
+        return new Position(x + dx, y + dy);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
